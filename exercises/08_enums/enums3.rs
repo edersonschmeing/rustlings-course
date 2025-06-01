e3ed12f35c97aae0a94b5ec42ddfc4ad0a6a79fe
edersonschmeing <edersonschmeing@gmail.com>
2025-06-01 19:12:56 -0300

    enums

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -46,11 +46,39 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        // DONE
+        match message {
+            Message::Resize{width, height} => self.resize(width, height),  
+            Message::Move(point) => self.move_position(point),   
+            Message::Echo(string) => self.echo(string), 
+            Message::ChangeColor(red, green, blue) => self.change_color(red, green, blue), 
+            Message::Quit => self.quit(),
+         }
     }
 }
 
 fn main() {
     // You can optionally experiment here.
+    let mut state = State {
+        width: 0,
+        height: 0,
+        position: Point { x: 0, y: 0 },
+        message: String::from("hello world"),
+        color: (0, 0, 0),
+        quit: false,
+    };
+
+    state.process(Message::Resize {
+        width: 10,
+        height: 30,
+    });
+    state.process(Message::Move(Point { x: 10, y: 15 }));
+    state.process(Message::Echo(String::from("Hello world!")));
+    state.process(Message::ChangeColor(255, 0, 255));
+    state.process(Message::Quit);
+
+    println!("{}", state.message)
+
 }
 
 #[cfg(test)]

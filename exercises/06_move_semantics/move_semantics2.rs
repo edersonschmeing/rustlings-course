0229c9bd2d8ced0c9c34f7f204dcf24be64a09ab
edersonschmeing <edersonschmeing@gmail.com>
2025-06-01 17:20:25 -0300

    move semantics

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -8,6 +8,12 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
 
 fn main() {
     // You can optionally experiment here.
+    let vec0 = vec![22, 44, 66];
+
+    let vec1 = fill_vec(vec0.clone());
+
+    println!("{:?}", vec0);
+    println!("{:?}", vec1);
 }
 
 #[cfg(test)]
@@ -16,11 +22,12 @@ mod tests {
 
     // TODO: Make both vectors `vec0` and `vec1` accessible at the same time to
     // fix the compiler error in the test.
+    // DONE
     #[test]
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
 
-        let vec1 = fill_vec(vec0);
+        let vec1 = fill_vec(vec0.clone()); //add .clone()
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
--- exercises/06_move_semantics/move_semantics4.rs
+++ exercises/06_move_semantics/move_semantics4.rs
@@ -1,17 +1,28 @@
+fn move_semantics4() {
+    let mut x = Vec::new();
+    let y = &mut x;
+    y.push(42);
+    let z = &mut x;
+    z.push(13);
+    println!("{:?}", x);
+}
+
 fn main() {
     // You can optionally experiment here.
+    move_semantics4();
 }
 
 #[cfg(test)]
 mod tests {
     // TODO: Fix the compiler errors only by reordering the lines in the test.
+    // DONE
     // Don't add, change or remove any line.
     #[test]
     fn move_semantics4() {
         let mut x = Vec::new();
         let y = &mut x;
-        let z = &mut x;
         y.push(42);
+        let z = &mut x;
         z.push(13);
         assert_eq!(x, [42, 13]);
     }
